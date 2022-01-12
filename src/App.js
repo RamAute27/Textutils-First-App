18caab014ca04b830a026475ed6f3fcee5a6702a
RamAute27 <ramaute2001@gmail.com>
2022-01-12 11:31:00 +0530

    first commit in first app

---
+++ src/App.js
@@ -0,0 +1,80 @@
+
+import './App.css';
+import Navebar from './components/Navebar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
+
+
+
+function App() {
+    const [mode, setMode] = useState('light');
+    const [alert, setAlert] = useState(null);
+
+    const showAlert = (message,type) => {
+      setAlert({
+        msg:message,
+        type: type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+
+      
+
+    }
+
+   const toggleMode= () => {
+     if(mode==='light'){
+       setMode('dark');
+       document.body.style.backgroundColor='#042743';
+       showAlert("Dark Mode has been enabled","success");
+     }
+     else{
+       setMode('light');
+       document.body.style.backgroundColor='white';
+       showAlert("Light Mode has been enabled","success");
+       
+     }
+
+    }
+    return(
+    <>
+    <Router>
+    
+   <Navebar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+   <Alert alert={alert}/>
+   <div className="container my-3">
+   <Switch>
+        
+          <Route exact path="/about">
+            <About/>
+          </Route>
+          <Route exact path="/">
+             <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />
+          </Route>
+   </Switch>
+    </div>
+  </Router>
+   </>  
+  
+   
+    
+    
+  
+
+
+
+  );
+}
+
+export default App;
+
